37b966405a4f34cd8c7fb7a4c7e939b18da24ea0
cyyan17 <cheny.17@163.com>
2018-08-03 16:58:44 +0800

    first time

---
+++ account/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0003_auto_20180721_1504'),
+        ('account', '0003_auto_20180803_1145'),
+    ]
+
+    operations = [
+    ]
